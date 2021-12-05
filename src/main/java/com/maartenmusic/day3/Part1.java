0b38c1ea61e837df0cd444db3413d8d660a774c7
maartenbakker <maartenbakker@gmail.com>
2021-12-05 14:32:14 +0100

    bugfix and refactor

--- src/main/java/com/maartenmusic/day3/Part1.java
+++ src/main/java/com/maartenmusic/day3/Part1.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.Map;
 
 public class Part1 {
-    private static Map<Integer, Integer> zeros = new HashMap<>();
-    private static Map<Integer, Integer> ones = new HashMap<>();
-    private static String gammaRate = "";
-    private static String epsilonRate = "";
+    private static final Map<Integer, Integer> zeros = new HashMap<>();
+    private static final Map<Integer, Integer> ones = new HashMap<>();
+    private static final StringBuilder gammaRate = new StringBuilder();
+    private static final StringBuilder epsilonRate = new StringBuilder();
 
     public static void main(String[] args) {
         List<String> data = InputData.getData();
@@ -22,19 +22,14 @@ public class Part1 {
                     updateOnes(i);
                 }
             }
-
-            for (int i = 0; i < binSize; i++) {
-                if (zeros.get(i) > ones.get(i)) {
-                    gammaRate += "0";
-                    epsilonRate += "1";
-                } else {
-                    gammaRate += "1";
-                    epsilonRate += "0";
-                }
-            }
         }
 
-        System.out.println(Integer.parseInt(gammaRate, 2) * Integer.parseInt(epsilonRate, 2));
+        for (int i = 0; i < binSize; i++) {
+            gammaRate.append(zeros.get(i) > ones.get(i) ? "0" : "1");
+            epsilonRate.append(zeros.get(i) > ones.get(i) ? "1" : "0");
+        }
+
+        System.out.println(Integer.parseInt(gammaRate.toString(), 2) * Integer.parseInt(epsilonRate.toString(), 2));
     }
 
     private static void updateZeros(int i) {
